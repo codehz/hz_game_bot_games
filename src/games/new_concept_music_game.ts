e1057cbff874e6708e83839625147d9694eee49c
codehz <codehz@outlook.com>
2022-02-01 14:47:32 +0800

    add box

--- src/games/new_concept_music_game.ts
+++ src/games/new_concept_music_game.ts
@@ -113,16 +113,41 @@ document.head.appendChild(css`
     height: 25vmin;
     transform: translate(calc(var(--x) * 25vmin), calc(var(--y) * 25vmin));
     display: block;
+    opacity: 1;
+    transition: transform linear 0.1s, opacity ease 0.5s;
+  }
+  game-cell::before,
+  game-cell::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+  }
+  game-cell::before {
+    inset: 2%;
+    clip-path: polygon(
+      10% 0,
+      0 10%,
+      0 90%,
+      10% 100%,
+      90% 100%,
+      100% 90%,
+      100% 10%,
+      90% 0
+    );
+    background: #0001;
+  }
+  game-cell::after {
+    transition: all ease 0.5s;
     background-image: url("data:image/svg+xml,%3Csvg width='48' height='48' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M41.42 7.309s3.885-1.515 3.56 2.164c-.107 1.515-1.078 6.818-1.834 12.553l-2.59 16.99s-.216 2.489-2.159 2.922c-1.942.432-4.856-1.515-5.396-1.948-.432-.325-8.094-5.195-10.792-7.575-.756-.65-1.62-1.948.108-3.463L33.648 18.13c1.295-1.298 2.59-4.328-2.806-.649l-15.11 10.28s-1.727 1.083-4.964.109l-7.016-2.165s-2.59-1.623 1.835-3.246c10.793-5.086 24.068-10.28 35.831-15.15z' fill='%23cc2307'/%3E%3C/svg%3E");
     background-size: 40%;
     background-position: center;
     background-repeat: no-repeat;
-    opacity: 1;
-    transition: all linear 0.1s;
   }
   game-cell.hidden {
-    background-size: 100%;
     opacity: 0;
+  }
+  game-cell.hidden::after {
+    background-size: 100%;
   }
   .game-title {
     display: flex;
