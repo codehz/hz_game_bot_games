cc3cc0c5abecdf2d40177e0d337eb27f5cdd6a0e
CodeHz <codehz@outlook.com>
2022-02-12 15:35:59 +0800

    refactor ecs

---
+++ src/ecs.test.ts
@@ -0,0 +1,20 @@
+import World from "./ecs.js";
+
+const world = new World({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
+
+const pv = world.view("position");
+
+const obj = world.add();
+
+console.log("before",...pv);
+
+obj.$position.x = 1;
+
+console.log("set $",...pv);
+
+delete obj.position;
+
+console.log("delete",...pv);
--- src/ecs.ts
+++ src/ecs.ts
@@ -1,99 +1,142 @@
-class View<C extends Record<string, any>> {
+interface View {
+  readonly interests: string[];
+  try_add(obj: object): void;
+  remove(obj: object): void;
+}
+
+class SimpleView<C extends Record<string, any>> implements View {
   #required: string[];
-  #list: Map<{}, C> = new Map();
+  #data: Set<C> = new Set();
 
-  constructor(...keys: string[]) {
-    this.#required = keys;
+  constructor(...required: string[]) {
+    this.#required = required;
   }
 
-  values() {
-    return this.#list.values();
+  get interests(): string[] {
+    return this.#required;
   }
 
-  try_add(key: {}, fetcher: (key: string) => any) {
-    const tmp: any = {};
-    for (const required of this.#required) {
-      const val = fetcher(required);
-      if (!val) return;
-      tmp[required] = val;
+  *[Symbol.iterator](): Iterator<Readonly<C>> {
+    yield* this.#data;
+  }
+
+  try_add(obj: C) {
+    if (this.#data.has(obj)) return;
+    if (this.#required.every((key) => key in obj)) {
+      this.#data.add(obj);
     }
-    this.#list.set(key, tmp);
   }
 
-  try_remove(key: {}, fetcher: (key: string) => any) {
-    for (const required of this.#required) {
-      const val = fetcher(required);
-      if (!val) {
-        this.#list.delete(key);
-        return;
-      }
-    }
+  remove(obj: C) {
+    this.#data.delete(obj);
   }
 }
 
+type AutoProp<T extends Record<string, any>> = {
+  readonly [K in `$${keyof T & string}`]: K extends `$${infer RK}`
+    ? Partial<T[RK]>
+    : never;
+};
+
 export default class World<C extends Record<string, any>> {
-  #storage: {
-    [key in keyof C]: Map<{}, C[key]>;
-  } = {} as any;
-  #views: View<any>[] = [];
+  #template: C;
+  #entities: Map<object, Partial<C> & AutoProp<C>> = new Map();
+  #views: View[] = [];
   #viewref: {
-    [key in keyof C]: View<any>[];
+    [key in keyof C]: View[];
   } = {} as any;
 
-  constructor(...names: (keyof C)[]) {
-    for (const name of names) {
-      this.#storage[name as keyof C] = new Map();
-      this.#viewref[name as keyof C] = [];
+  constructor(template: C) {
+    this.#template = template;
+    for (const name in template) {
+      this.#viewref[name] = [];
     }
   }
 
-  #handler: ProxyHandler<{}> = {
-    get: (target: {}, name: string) => this.#storage[name].get(target),
-    deleteProperty: (target: {}, name: string) => {
-      if (!this.#storage[name].delete(target)) return false;
-      for (const ref of this.#viewref[name])
-        ref.try_remove(target, (name) => this.#storage[name].get(target));
-      return true;
-    },
-    defineProperty: () => false,
-    getOwnPropertyDescriptor: () => undefined,
+  #handler: ProxyHandler<object> = {
     getPrototypeOf: (target) => target,
-    has: (target, name: string) => this.#storage[name].has(target),
-    isExtensible: () => true,
-    ownKeys: (target) =>
-      Object.entries(this.#storage)
-        .filter(([, map]) => map.has(target))
-        .map(([name]) => name),
-    preventExtensions: () => false,
-    set: (target: {}, name: string, value: any) => {
-      this.#storage[name].set(target, value);
-      for (const ref of this.#viewref[name])
-        ref.try_add(target, (name) => this.#storage[name].get(target));
+    setPrototypeOf: () => false,
+    get: (target, key) => {
+      if (typeof key == "symbol") return undefined;
+      if (key.startsWith("$")) {
+        const rk = key.slice(1);
+        if (rk in target) return Reflect.get(target, rk);
+        const ret = Object.create(this.#template[rk]);
+        Reflect.set(target, rk, ret);
+        for (const view of this.#viewref[rk]) {
+          view.try_add(target);
+        }
+        return ret;
+      } else {
+        return Reflect.get(target, key);
+      }
+    },
+    set: (target, key, value) => {
+      if (typeof key == "symbol") return false;
+      if (key.startsWith("$")) throw new TypeError("readonly view");
+      console.assert(value != null);
+      const skipUpdate = key in target;
+      Reflect.set(target, key, value);
+      if (!skipUpdate)
+        for (const view of this.#viewref[key]) view.try_add(target);
       return true;
     },
-    setPrototypeOf: () => false,
+    deleteProperty: (target, key) => {
+      if (typeof key == "symbol") return false;
+      if (key.startsWith("$")) throw new TypeError("readonly view");
+      if (key in target) {
+        Reflect.deleteProperty(target, key);
+        for (const view of this.#viewref[key]) view.remove(target);
+        return true;
+      }
+      return false;
+    },
   };
 
-  create(): Partial<C> {
-    return new Proxy(Object.create(null), this.#handler);
+  add(obj: Partial<C> = Object.create(null)): Partial<C> & AutoProp<C> {
+    Object.setPrototypeOf(obj, null);
+    for (const key in obj) {
+      for (const view of this.#viewref[key]) {
+        view.try_add(obj);
+      }
+    }
+    const proxy = new Proxy(obj, this.#handler) as Partial<C> & AutoProp<C>;
+    this.#entities.set(obj, proxy);
+    return proxy;
   }
 
-  remove(entity: Partial<C>) {
-    const key = Object.getPrototypeOf(entity) as {};
-    for (const [, map] of Object.entries(this.#storage)) {
-      map.delete(key);
-    }
+  remove(obj: Partial<C>) {
     for (const view of this.#views) {
-      view.try_remove(key, () => false);
+      view.remove(obj);
     }
+    this.#entities.delete(obj);
   }
 
-  view<K extends keyof C>(...names: K[]): View<Pick<C, K>> {
-    const ret = new View<Pick<C, K>>(...names as any);
-    this.#views.push(ret);
-    for (const name of names) {
-      this.#viewref[name].push(ret);
-    }
+  get(obj: object): (Partial<C> & AutoProp<C>) | undefined {
+    return this.#entities.get(obj);
+  }
+
+  view<V extends string & keyof C>(...keys: V[]): SimpleView<Pick<C, V>> {
+    const ret = new SimpleView<Pick<C, V>>(...keys);
+    this.view_by(ret);
     return ret;
+  }
+
+  view_by(view: View) {
+    this.#views.push(view);
+    for (const key of view.interests) this.#viewref[key].push(view);
+    for (const [ent] of this.#entities) view.try_add(ent as any);
+  }
+
+  remove_view(view: View) {
+    const idx = this.#views.indexOf(view);
+    if (idx != -1) {
+      this.#views.splice(idx, 1);
+      for (const key of view.interests) {
+        const list = this.#viewref[key];
+        const idx = list.indexOf(view);
+        list.splice(idx, 1);
+      }
+    }
   }
 }
